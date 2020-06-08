89b42bdbe80efad2ebb703454a4ac7b531f49cce
RyunosukeHorihata <ryunosuke.horihata@gmail.com>
2020-06-09 00:53:33 +0900

    add index action in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    @accounts = Account.where(activated: true).paginate(page: params[:page]).search(params[:search])
+  end
 end
