471ca46932a60e45da51573e5d7be45c7b91bc85
RyunosukeHorihata <ryunosuke.horihata@gmail.com>
2020-06-13 08:57:11 +0900

    applicationコントローラーのindexアクションの定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    @accounts = Account.where(activated: true).paginate(page: params[:page]).search(params[:search])
+    # @accounts = Account.where(activated: true).paginate(page: params[:page]).search(params[:search])
+    @accounts = Account.search(params[:search])
   end
+
 end
