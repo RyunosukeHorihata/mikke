57fe53312babe45fb6c6f73c0c28c4eb048c5f2d
RyunosukeHorihata <ryunosuke.horihata@gmail.com>
2020-06-13 20:06:17 +0900

    accountコントローラーのindexアクション定義

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,6 +2,6 @@ class AccountController < ApplicationController
   # before_action :move_to_index, except: [:index, :show, :search]
 
   def index
-    
+    @accounts = Account.search(params[:search])
   end
 end
