65cc2649d4e1a3081239e70adc80a4b2d021fa41
RyunosukeHorihata <ryunosuke.horihata@gmail.com>
2020-06-25 00:01:41 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def index
     # @accounts = Account.where(activated: true).paginate(page: params[:page]).search(params[:search])
-    @accounts = Account.search(params[:search])
+    # @accounts = Account.search(params[:search])
   end
 
 end
