d8b07b9bfc07705068bffddfc09cc9768f85cd5f
RyunosukeHorihata <ryunosuke.horihata@gmail.com>
2020-06-28 15:05:03 +0900

    favicon及びOGP

--- Gemfile
+++ Gemfile
@@ -62,3 +62,4 @@ end
 gem 'dotenv-rails'
 gem 'twitter'
 gem 'pry-rails'
+gem 'meta-tags'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'mikke',
+      title: '',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'キーワードを入力するだけでお気に入りのTwitterアカウントが見つかります！今や主要メディアは「個」の時代です。有益な情報を集める手段として、Twitterアカウントをフォローしてみてはいかがでしょうか？',
+      keywords: 'Twitter,Twitterアカウント,情報収集,インフルエンサー',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('assets/images/favicon.ico')},
+        { href: image_url('assets/images/logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('assets/images/top.png'),
+        local: 'ja-JP',
+      },
+      twitter: {
+        card: 'summary',
+        site: '@nosukee',
+        # image: asset_pack_url('assets/images/twitter_icon.png'),
+        # width: 100,
+        # height: 100
+      }
+    }
+  end
 end
